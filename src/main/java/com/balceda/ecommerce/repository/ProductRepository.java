80d3422b6148b8b2f09615467ddc1d92aebd38a7
Juan Balceda <juanbalceda@gmail.com>
2020-08-05 23:56:24 +0200

    Added cross origin support to rest api

--- src/main/java/com/balceda/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/balceda/ecommerce/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.balceda.ecommerce.repository;
 
 import com.balceda.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
