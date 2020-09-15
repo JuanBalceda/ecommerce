0823d76defa6b9d566c4d0c1875d205985fae807
Juan Balceda <juanbalceda@gmail.com>
2020-09-16 00:39:37 +0200

    Exposing entities ids on Spring Data REST

--- src/main/java/com/balceda/ecommerce/config/DataRestConfig.java
+++ src/main/java/com/balceda/ecommerce/config/DataRestConfig.java
@@ -2,13 +2,24 @@ package com.balceda.ecommerce.config;
 
 import com.balceda.ecommerce.entity.Product;
 import com.balceda.ecommerce.entity.ProductCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+
 @Configuration
 public class DataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public DataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
@@ -24,5 +35,16 @@ public class DataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure(((metadata, httpMethods) -> httpMethods.disable(unsupportedActions)))
                 .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions));
+
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+
+        Class[] domainTypes = entityManager.getMetamodel().getEntities().stream()
+                .map(EntityType::getJavaType)
+                .toArray(Class[]::new);
+
+        config.exposeIdsFor(domainTypes);
     }
 }
